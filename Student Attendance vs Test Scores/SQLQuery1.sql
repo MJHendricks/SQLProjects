e8e82e2d05bd9fdc5c8225f3e0639321ee75f05b
MJHendricks <mattjhcontact@gmail.com>
2024-05-31 15:04:37 +0200

    update SQL

--- Student Attendance vs Test Scores/SQLQuery1.sql
+++ Student Attendance vs Test Scores/SQLQuery1.sql
@@ -1,4 +1,23 @@
-SELECT TOP (1000) [student_name]
-      ,[attendance_percentage]
-      ,[F3]
-  FROM ['Attendance Figures$']
+  -- join datasets
+
+SELECT stu.F1, att.student_name, att.attendance_percentage, stu.F2, stu.F3, stu.F4
+INTO MergedDatasets
+FROM ['Attendance Figures$'] att
+JOIN ['Student Test Scores$'] stu
+ON att.[student_name] = stu.F2;
+
+SELECT * FROM MergedDatasets
+
+-- correct spelling mistakes
+
+-- Determine The source of spelling errors in the subject column
+SELECT DISTINCT(F3) FROM MergedDatasets;
+--"Engish" should be "English", "Sciece" should be "Science"
+
+UPDATE MergedDatasets
+SET F3 = REPLACE ( F3, 'Engish', 'English');
+
+UPDATE MergedDatasets
+SET F3 = REPLACE ( F3, 'Sciece', 'Science');
+
+
